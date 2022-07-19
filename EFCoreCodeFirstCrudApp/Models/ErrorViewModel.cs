9d54e9aed2ef373e43381850fba7dcf734eb35db
mebpin <bipin.timalsina22@gmail.com>
2022-07-19 21:36:09 +0545

    Add project files.

---
+++ EFCoreCodeFirstCrudApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EFCoreCodeFirstCrudApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
